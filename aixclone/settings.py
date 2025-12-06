4711ab95de30bf5f884758ac5f5b2905334ad44b
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-12-06 17:19:35 +0530

    Update settings.py

--- aixclone/settings.py
+++ aixclone/settings.py
@@ -1,144 +1,140 @@
 from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 import environ
 import dj_database_url
 from dotenv import load_dotenv
-# Application definition
-AWS_STORAGE_BUCKET_NAME ='shaurya1234512'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_S3_FILE_OVERWRITE = True
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 load_dotenv()
 env = environ.Env()
 environ.Env.read_env()
 
-SECRET_KEY = 'your-secret-key'
-
-DEBUG = True
+SECRET_KEY = env("SECRET_KEY", default="dev-secret-key")
+DEBUG = env("DEBUG", default="True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
+# ------------------------------------------------------------
+# APPLICATIONS
+# ------------------------------------------------------------
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'tools',
-    'aixclone',
-]
-LOGOUT_REDIRECT_URL = 'home'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
+    "tools",
+    "aixclone",
+]
+
+# Enable S3 only when USE_S3=1
+USE_S3 = os.getenv("USE_S3", "0") == "1"
+if USE_S3:
+    INSTALLED_APPS.append("storages")
+
+# ------------------------------------------------------------
+# MIDDLEWARE
+# ------------------------------------------------------------
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'aixclone.urls'
+ROOT_URLCONF = "aixclone.urls"
 
+# ------------------------------------------------------------
+# TEMPLATES
+# ------------------------------------------------------------
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'aixclone.wsgi.application'
+WSGI_APPLICATION = "aixclone.wsgi.application"
 
-import dj_database_url
-DATABASES={
-    'default':dj_database_url.parse(env('DATABASE_URL'))
+# ------------------------------------------------------------
+# DATABASE
+# ------------------------------------------------------------
+DATABASES = {
+    "default": dj_database_url.parse(env("DATABASE_URL"))
 }
 
+# ------------------------------------------------------------
+# PASSWORD VALIDATION
+# ------------------------------------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+# ------------------------------------------------------------
+# INTERNATIONALIZATION
+# ------------------------------------------------------------
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
-
-USE_L10N = True
-
 USE_TZ = True
 
+# ------------------------------------------------------------
+# STATIC & MEDIA CONFIG
+# ------------------------------------------------------------
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/login/'          # redirect to login if @login_required
-LOGIN_REDIRECT_URL = '/'       # redirect after login
-LOGOUT_REDIRECT_URL = '/'      # redirect after logout
+AWS_STORAGE_BUCKET_NAME = "shaurya1234512"
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
 
+# DEFAULT: Local development (not S3)
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "tools" / "static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-# Development: Use local storage for static files
-# Production: Uncomment the S3 storage below
 STORAGES = {
-    "default": {
-        "BACKEND": "tools.storage.MediaStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"},
 }
 
-# Local development URLs
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "tools" / "static"]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-# Use local filesystem for uploaded media in development
-if DEBUG:
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-else:
-    # In production, use S3 for media (keep existing STORAGES/S3 config)
-    DEFAULT_FILE_STORAGE = 'tools.storage.MediaStorage'
+# ------------------------------------------------------------
+# SWITCH TO S3 IF USE_S3=1
+# ------------------------------------------------------------
+if USE_S3:
+    STORAGES = {
+        "default": {"BACKEND": "tools.storage.MediaStorage"},
+        "staticfiles": {"BACKEND": "tools.storage.StaticStorage"},
+    }
 
-# Production: Uncomment below for S3 storage
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "tools.storage.MediaStorage",
-#     },
-#     "staticfiles": {
-#         "BACKEND": "tools.storage.StaticStorage",
-#     },
-# }
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-# STATICFILES_DIRS = [BASE_DIR / "tools" / "static"]
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+
+# ------------------------------------------------------------
+# AUTH REDIRECTS
+# ------------------------------------------------------------
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
